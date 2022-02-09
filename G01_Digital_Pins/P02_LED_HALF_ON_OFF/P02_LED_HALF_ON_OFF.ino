7322603ff582437591f89e18b0ebe34d8ff63d8f
Sayed Fadhel Ali <sfadnet@gmail.com>
2022-02-09 16:59:05 +0300

    modify and adjust

--- G01_Digital_Pins/P02_LED_HALF_ON_OFF/P02_LED_HALF_ON_OFF.ino
+++ G01_Digital_Pins/P02_LED_HALF_ON_OFF/P02_LED_HALF_ON_OFF.ino
@@ -8,7 +8,7 @@
 
 void setup() {
    DDRB = 0b00111111;  //set pins 0-6 of PORT B as OUTPUT
-   PORTB = 0b010101;  //set pins 0-6 to HIGH
+   PORTB = 0b010101;  //set half of the pins 0-6 to HIGH
    delay(500);        //wait 500ms (0.5 second)
 }
 
---
+++ G01_Digital_Pins/P10_INPUT_INTERRUPT/P10_INPUT_INTERRUPT.ino
@@ -0,0 +1,18 @@
+/*
+   Board: Arduino UNO
+   
+   Poject Description: Read digital input on PORT #2 if HIGH then
+   TURN ON ALL LEDs of PORT B otherwise TURN them OFF.
+*/
+
+void setup() {
+   DDRB = 0b00111111;    //set pins 0-6 of PORT B as OUTPUT
+   pinMode(2, INPUT);   //set pins #2 as INPUT
+}
+
+void loop() {
+   if(digitalRead(2) == HIGH)
+      PORTB = 0xFF;
+   else
+      PORTB = 0;
+}
