3d5e8619bd4c3809d0c556da5fdc30a416023e32
Sayed Fadhel Ali <sfadnet@gmail.com>
2022-02-13 18:31:42 +0300

    make it pull up

--- G01_Digital_Pins/G01_P08_INPUT_PUSH_BUTTON/G01_P08_INPUT_PUSH_BUTTON.ino
+++ G01_Digital_Pins/G01_P08_INPUT_PUSH_BUTTON/G01_P08_INPUT_PUSH_BUTTON.ino
@@ -1,9 +1,9 @@
 /*
    Board: Arduino UNO
    
-   Poject Description: Read digital input on PORT #2 if HIGH then
+   Poject Description: Read digital input on PORT #2 if LOW then
    TURN ON ALL LEDs of PORT B otherwise TURN them OFF.
-
+   
    url: https://github.com/sfad/arduino_projects
 */
 
@@ -13,7 +13,7 @@ void setup() {
 }
 
 void loop() {
-   if(digitalRead(2) == HIGH)
+   if(digitalRead(2) == LOW)
       PORTB = 0xFF;
    else
       PORTB = 0;
