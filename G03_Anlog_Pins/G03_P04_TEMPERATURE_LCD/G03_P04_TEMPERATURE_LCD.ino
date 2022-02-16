8eb6fd2e2398c529d725a6634024a170928e0c6f
Sayed Fadhel Ali <sfadnet@gmail.com>
2022-02-16 09:52:06 +0300

    add temperature LCD project

---
+++ G03_Anlog_Pins/G03_P04_TEMPERATURE_LCD/G03_P04_TEMPERATURE_LCD.ino
@@ -0,0 +1,57 @@
+/*
+	Board: Arduino UNO
+   
+	Poject Description: Measure temperature using LM35
+		and output the result on LCD. Button Used to
+		change the Temperature mode between C/F
+
+	url: https://github.com/sfad/arduino_projects
+*/
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define BUTTON 2
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // run ic2_scanner sketch and get the IC2
+
+char tempMode = 'c';
+
+void setup() {
+	lcd.backlight();
+	lcd.init();			//intialize the LCD
+	lcd.setCursor(2, 0);		//set the cursor to Column 3, Row 1
+	lcd.print("TEMPERATURE:");	//Print a message to the LCD.
+	pinMode(BUTTON, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(BUTTON), changeMode, FALLING);
+}
+
+void loop() {
+	printTemp(tempMode);
+	delay(1000);
+}
+
+void changeMode() {
+	tempMode = tempMode == 'c' ? 'f' : 'c';
+}
+
+float getTemp(char mode) {
+	int val = analogRead(A0);
+	float mv = (val/1023.0) * 5000;
+	float cel = mv/10;
+	float fahr = (cel*9)/5 + 32;
+	return mode == 'f' ? fahr : cel;
+}
+
+void printTemp(char mode) {
+	clearLine2();
+	lcd.setCursor(2, 1);		//set the cursor to Column 3, Row 2
+	float temperature = getTemp(mode);
+	lcd.print(temperature);
+	lcd.print(mode=='f' ? " F" : " C");
+}
+
+void clearLine2() {
+	lcd.setCursor(2, 1);		//set the cursor to Column 3, Row 2
+	lcd.print("                "); //print 16 spaces to LCD
+}
